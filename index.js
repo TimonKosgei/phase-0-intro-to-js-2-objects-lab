887fcf272e0348d3c1a997b921713108b240a038
TimonKosgei <timonkosgei15209@gmail.com>
2024-09-25 12:20:46 +0300

    Did labs

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {
+    'name':'Timon',
+    'streetAddress':'1040 Eldoret'
+}
+function updateEmployeeWithKeyAndValue(object,key,value){
+    //use spread operator
+    return {...object,[key]:value}
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(object,key,value){
+    object[key] = value;
+    return object;
+}
+function deleteFromEmployeeByKey(object,key){
+    const newObject = {...object};
+    delete newObject[key];
+    return newObject;
+}
+function destructivelyDeleteFromEmployeeByKey(object,key){
+    delete object[key];
+    return object;
+}
